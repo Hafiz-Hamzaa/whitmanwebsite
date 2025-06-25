b9070fbead2cc9caea761cdc9fa11e8ee651747d
Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2025-06-25 17:19:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+    window.addEventListener('DOMContentLoaded', () => {
+    // Show after 3 seconds
+    setTimeout(() => {
+      document.getElementById('popup').style.display = 'flex';
+    }, 4000);
+
+    // Close on ❌ click
+    document.getElementById('popupClose').addEventListener('click', () => {
+      document.getElementById('popup').style.display = 'none';
+    });
+  });
+
+
+
+  const hamburger = document.getElementById("hamburger");
+  const navContainer = document.getElementById("nav-container");
+
+  hamburger.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+  });
